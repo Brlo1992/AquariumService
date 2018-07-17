a87f399493aa810b68c9e9d20b4b8b569ea710b4
dawid krzus <dkrzus92@gmail.com>
2018-07-17 12:32:43 +0200

    Add sources

---
+++ OZE.AquariumService/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OZE.AquariumService
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (var client = new HttpClient())
+            {
+                await client.GetAsync("http://192.168.8.133/turnOn");
+
+                Thread.Sleep(10000);
+
+                await client.GetAsync("http://192.168.8.133/turnOff");
+            }
+        }   
+    }
+}
